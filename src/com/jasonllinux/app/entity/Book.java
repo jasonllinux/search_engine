55e309ffa499cf2c7a6f9a6e745d47f0b2369af8
jasonllinux <jasonllinux@gmail.com>
2012-09-12 16:50:40 +0800

    initial

---
+++ src/com/jasonllinux/app/entity/Book.java
@@ -0,0 +1,68 @@
+package com.jasonllinux.app.entity;
+
+public class Book {
+
+	private Integer id;
+
+	/**
+	 * 书名
+	 */
+	private String title;
+
+	/**
+	 * 内容
+	 */
+	private String content;
+
+	/**
+	 * 作者
+	 */
+	private String author;
+
+	public Book(Integer id, String title, String content, String author) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.author = author;
+	}
+	
+	public Book () {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+	
+
+}
---
+++ src/com/jasonllinux/app/se/TestSearch.java
@@ -0,0 +1,13 @@
+package com.jasonllinux.app.se;
+
+public class TestSearch {
+
+	public static void main(String[] args) {
+		
+		IndexFiles index = new IndexFiles();
+		
+
+	}
+	
+
+}
